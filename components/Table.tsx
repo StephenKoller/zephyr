8d49b4b97e0468f2eff8f33fb231a89603c183ca
Stephen Koller <stephenkoller@gmail.com>
2020-02-28 21:41:29 -0500

    Convert function body to implicit return

--- components/Table.tsx
+++ components/Table.tsx
@@ -5,74 +5,72 @@ interface Props {
   forecast: Forecast
 }
 
+// format metric to imperial
+// TODO: move data transformation to API layer
 const formatPercentage = (num: number) => (num * 100).toFixed(0)
-
 const formatTime = (time: number) => {
   const t = new Date(time * 1000)
   return t.toLocaleString()
 }
-
 const formatTemperature = (temp: number) => ((temp * 9) / 5 + 32).toFixed(0)
 const formatSpeed = (speed: number) => (speed * 2.237).toFixed(0)
 
-const Table: FC<Props> = ({ forecast }) => {
-  return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>Summary</th>
-            <th>Chance of Precipitation</th>
-            <th>Temperature</th>
-            <th>Humidity</th>
-            <th>Wind Speed</th>
-            <th>Wind Gusts</th>
+const Table: FC<Props> = ({ forecast }) => (
+  <>
+    <table>
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th>Summary</th>
+          <th>Chance of Precipitation</th>
+          <th>Temperature</th>
+          <th>Humidity</th>
+          <th>Wind Speed</th>
+          <th>Wind Gusts</th>
+        </tr>
+      </thead>
+      <tbody>
+        {forecast.hourly?.data?.map((hour: HourlyDataPoint) => (
+          <tr key={hour.time}>
+            <td>{formatTime(hour.time)}</td>
+            <td>{hour.summary}</td>
+            <td>{formatPercentage(hour.precipProbability)}%</td>
+            <td>{formatTemperature(hour.temperature)}º F</td>
+            <td>{formatPercentage(hour.humidity)}%</td>
+            <td>{formatSpeed(hour.windSpeed)} MPH</td>
+            <td>{formatSpeed(hour.windGust)} MPH</td>
           </tr>
-        </thead>
-        <tbody>
-          {forecast.hourly?.data?.map((hour: HourlyDataPoint) => (
-            <tr key={hour.time}>
-              <td>{formatTime(hour.time)}</td>
-              <td>{hour.summary}</td>
-              <td>{formatPercentage(hour.precipProbability)}%</td>
-              <td>{formatTemperature(hour.temperature)}º F</td>
-              <td>{formatPercentage(hour.humidity)}%</td>
-              <td>{formatSpeed(hour.windSpeed)} MPH</td>
-              <td>{formatSpeed(hour.windGust)} MPH</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <style jsx>{`
-        table {
-          border-spacing: 0;
-        }
+        ))}
+      </tbody>
+    </table>
+    <style jsx>{`
+      table {
+        border-spacing: 0;
+      }
 
-        tr {
-          background-color: #eee;
-        }
+      tr {
+        background-color: #eee;
+      }
 
-        tr:nth-of-type(2n) {
-          background-color: #ddd;
-        }
+      tr:nth-of-type(2n) {
+        background-color: #ddd;
+      }
 
-        tr :last-child td {
-          border-bottom: 0;
-        }
+      tr :last-child td {
+        border-bottom: 0;
+      }
 
-        th,
-        td {
-          margin: 0;
-          padding: 0.5rem;
-        }
+      th,
+      td {
+        margin: 0;
+        padding: 0.5rem;
+      }
 
-        td:last-child {
-          border-right: 0;
-        }
-      `}</style>
-    </>
-  )
-}
+      td:last-child {
+        border-right: 0;
+      }
+    `}</style>
+  </>
+)
 
 export default Table
