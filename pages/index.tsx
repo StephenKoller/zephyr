3524e76c6db4b068e00dc9e8aa73349bd8d42423
Stephen Koller <stephenkoller@gmail.com>
2020-03-01 00:44:55 -0500

    Move styles back into index page component

--- pages/index.tsx
+++ pages/index.tsx
@@ -175,8 +175,24 @@ const IndexPage: NextPage<Props> = () => {
 
       <style jsx>{indexStyles}</style>
       <style jsx>{`
-        #location-search {
-          background-color: ${error ? 'red' : '#fff'};
+        h1 {
+          color: #2e5689;
+          font-family: 'Caesar Dressing', Arial, sans-serif;
+          font-size: 4rem;
+          font-weight: 500;
+          letter-spacing: 0.5rem;
+          margin: 2rem 0 1rem 0;
+        }
+
+        h2 {
+          font-weight: 100;
+        }
+
+        #search-button {
+          padding: 0.3em 1em;
+          font-size: 1em;
+          border-radius: 4px;
+          margin: 0.5em;
         }
       `}</style>
     </Layout>
