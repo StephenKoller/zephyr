f674102ddaf0f9688a28d46ffe63a77ef7dbcbd3
Stephen Koller <stephenkoller@gmail.com>
2020-03-01 20:48:02 -0500

    Rearrange summary

--- components/ForecastSummary.tsx
+++ components/ForecastSummary.tsx
@@ -8,28 +8,6 @@ interface Props {
 
 const ForecastSummary: React.FC<Props> = ({ suntimes, forecast }) => (
   <div className="container">
-    {suntimes?.results?.sunrise && (
-      <div className="columns">
-        <div className="column col-2">
-          <h5>Sunrise:</h5>
-        </div>
-        <div className="column col-10">
-          <h5>{suntimes?.results?.sunrise}</h5>
-        </div>
-      </div>
-    )}
-
-    {suntimes?.results?.sunset && (
-      <div className="columns">
-        <div className="column col-2">
-          <h5>Sunset:</h5>
-        </div>
-        <div className="column col-10">
-          <h5>{suntimes?.results?.sunset}</h5>
-        </div>
-      </div>
-    )}
-
     {forecast?.currently?.summary && (
       <div className="columns">
         <div className="column col-2">
@@ -59,6 +37,28 @@ const ForecastSummary: React.FC<Props> = ({ suntimes, forecast }) => (
         </div>
         <div className="column col-10">
           <h5>{forecast?.daily?.summary}</h5>
+        </div>
+      </div>
+    )}
+
+    {suntimes?.results?.sunrise && (
+      <div className="columns">
+        <div className="column col-2">
+          <h5>Sunrise:</h5>
+        </div>
+        <div className="column col-10">
+          <h5>{suntimes?.results?.sunrise}</h5>
+        </div>
+      </div>
+    )}
+
+    {suntimes?.results?.sunset && (
+      <div className="columns">
+        <div className="column col-2">
+          <h5>Sunset:</h5>
+        </div>
+        <div className="column col-10">
+          <h5>{suntimes?.results?.sunset}</h5>
         </div>
       </div>
     )}
